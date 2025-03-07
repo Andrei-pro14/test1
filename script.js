6044648f05788a420050a799ce1e07fd84f37df0
Andrei <moisiucandrei06@gmail.com>
2025-03-07 16:25:22 +0200

    featured-am-adaugat-text

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const buton = document.querySelector("button")
 buton.addEventListener("click", e => {
     const div = document.createElement("div")
     div.classList.add("patrat")
+    const p = document.createElement("p")
+    p.textContent = "salut din webstorm"
 body.appendChild(div)
+    body.appendChild(p)
 })
