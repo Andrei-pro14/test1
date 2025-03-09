cb33f6b8929b7db37362a71672236b9c69a238ed
Andrei <moisiucandrei06@gmail.com>
2025-03-09 11:15:46 +0200

    featured-am-sters-din-js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const body = document.body
 const buton = document.querySelector("button")
 buton.addEventListener("click", e => {
-    const div = document.createElement("div")
-    div.classList.add("patrat")
-    const p = document.createElement("p")
-    p.textContent = "salut din webstorm"
-body.appendChild(div)
-    body.appendChild(p)
+    
 })
